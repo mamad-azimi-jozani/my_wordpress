30078a5ddee6b8a6d41b345ee3151ef8e7c23f1c
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-04-08 12:07:40 +0330

    learn famous loop in wordpress

---
+++ app/wp-content/themes/fictional-university/page.php
@@ -0,0 +1,10 @@
+<?php
+
+while(have_posts( )){
+    the_post( ); ?>
+    <h1>this is a page not a post</h1>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+<?php }
+?>
